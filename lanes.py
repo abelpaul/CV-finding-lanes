ead071a6183a94bcafeda77d0758d0cbb298d466
Abel Paul George <abel.george@cfacorp.com>
2020-01-08 10:23:28 -0500

    defined gradient function

--- lanes.py
+++ lanes.py
@@ -1,21 +1,22 @@
 import cv2
 import numpy as np
 
+# grayscale image, meaning one channel of color rather than 3 (in RGB)
+#Applying a Gaussian Blur
+#Computing the gradient (Calculating the derivative)
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    canny = cv2.Canny(blur, 50, 50)
+    return canny
+
 # reading image into a matrix
 img = cv2.imread("test_image.jpg")
 
 # creating an image copy so that matrices are not manipulated
 lane_image= np.copy(img)
 
-# grayscale image, meaning one channel of color rather than 3 (in RGB)
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-
-#Applying a Gaussian Blur
-blur = cv2.GaussianBlur(gray, (5,5), 0)
-
-#Computing the gradient (Calculating the derivative)
-canny = cv2.Canny(blur, 50, 50)
-
+canny = canny(lane_image)
 #strongest gradients are now outlined
 
 # showing image in a seperate window
