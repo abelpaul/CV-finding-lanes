98cb7f0678a250a1107fc47542979153c8b5ca14
Abel Paul George <abel.george@cfacorp.com>
2020-01-08 10:08:50 -0500

    added grayscaling

--- lanes.py
+++ lanes.py
@@ -1,6 +1,16 @@
 import cv2
+import numpy as np
 
+# reading image into a matrix
 img = cv2.imread("test_image.jpg")
 
-cv2.imshow('result', img)
+# creating an image copy so that matrices are not manipulated
+lane_image= np.copy(img)
+
+# grayscale iamge
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+
+# showing image in a seperate window
+cv2.imshow('result', gray)
 cv2.waitKey(0)
+
